662b415d7c9165db9d584dabeb925be45c2e102c
Rodriguez, Nick <Nick.Rodriguez@dhgllp.com>
2018-03-15 19:19:25 -0400

    Fix solution 6. Rename solution.

--- problem6.py
+++ problem6.py
@@ -1,15 +1,22 @@
-max_num = None
-min_num = None
+import math
 
-for i in input().split():
-    num = int(i)
-    if max_num == None or min_num == None:
-        max_num = num
-        min_num = num
-    else:
-        if num > max_num:
-            max_num = num
-        elif num < min_num:
-            min_num = num
+num_sets = int(input())
 
-print(max_num, min_num)
+results = []
+
+for i in range(0, num_sets):
+    for line in input().split("\n"):
+        a, b = map(int, line.split())
+
+        quotient = a / b
+        remainder = quotient % 1
+
+        if remainder >= 0.5:
+            rounded_num = math.ceil(quotient)
+        else:
+            rounded_num = a // b
+
+        #print(quotient, rounded_num)
+        results.append(rounded_num)
+
+print(" ".join(map(str, results)))
